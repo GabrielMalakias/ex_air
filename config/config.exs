acdcbf2347d8d01057e888cae57e792fd288f9b9
Gabriel Malaquias <5194287+GabrielMalakias@users.noreply.github.com>
2021-09-04 11:55:34 +0200

    basic docker config

--- config/config.exs
+++ config/config.exs
@@ -23,6 +23,12 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :ex_air, Friends.Repo,
+  database: "ex_air",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost"
+
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
